17918b71aec86aeab7e46554af83499ba57bdacf
ankurdixitofficial <ankurdixitofficial@gmail.com>
2025-02-24 19:56:34 +0530

    Update: describe your changes here

--- src/lib/mongodb-adapter.ts
+++ src/lib/mongodb-adapter.ts
@@ -1,10 +1,14 @@
 import { MongoClient } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MongoDB URI to .env.local');
+  throw new Error(
+    'Please define the MONGODB_URI environment variable. Check your .env.local file or Vercel environment variables.'
+  );
 }
 
 const uri = process.env.MONGODB_URI;
+const options = {};
+
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
@@ -16,14 +20,16 @@ if (process.env.NODE_ENV === 'development') {
   };
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri);
+    client = new MongoClient(uri, options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
 export default clientPromise; 
